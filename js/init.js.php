113d4bcac3c03d8b7dcc0a1f1cc67033a0241b83
sakanafurai <info@via.rossa.cc>
2025-02-11 21:38:09 +0900

    Copy scripts of mobile-menu.js into init.js.php to show mobile menu properly

--- js/init.js.php
+++ js/init.js.php
@@ -21,7 +21,30 @@ function appendCss(URL) {
 }
 
 if(isSmartPhone(true)) {
-  appendScript("<?php echo DOMAIN_THEME . 'js/mobile-menu.js' ?>");
+  //appendScript("<?php echo DOMAIN_THEME . 'js/mobile-menu.js' ?>");
+
+  document.addEventListener('DOMContentLoaded', () => {
+
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach( el2 => {
+      el2.addEventListener('click', () => {
+
+        // Get the target from the "data-target" attribute
+        const target = el2.dataset.target;
+        const $target = document.getElementById(target);
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el2.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
+      });
+    });
+
+  });
+
 } else {
   //appendScript("<?php echo DOMAIN_THEME . 'js/aos.js' ?>");
   appendCss("<?php echo DOMAIN_THEME . 'css/aos.css' ?>");

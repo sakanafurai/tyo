0807de6ac9d312c15c34c590f9eb658a46aafda5
sakanafurai <info@via.rossa.cc>
2025-02-11 21:46:40 +0900

    Replace the page folder icon from FontAwesome's to Lucide's

--- php/page.php
+++ php/page.php
@@ -33,7 +33,7 @@
       <!--Category tag-->
 
       <?php if ($page->category()): ?>
-        <span class="category-tag tag is-link"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M64 480H448c35.3 0 64-28.7 64-64V160c0-35.3-28.7-64-64-64H288c-10.1 0-19.6-4.7-25.6-12.8L243.2 57.6C231.1 41.5 212.1 32 192 32H64C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64z"/></svg><?php echo $page->category(); ?></span>
+        <span class="category-tag tag is-link"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-folder"><path d="M20 20a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2h-7.9a2 2 0 0 1-1.69-.9L9.6 3.9A2 2 0 0 0 7.93 3H4a2 2 0 0 0-2 2v13a2 2 0 0 0 2 2Z"/></svg><?php echo $page->category(); ?></span>
       <?php endif ?>
 
       <!-- Page tags -->

e4b9ccba43bd59edeefc968662da21c15a65675f
sakanafurai <info@via.rossa.cc>
2025-02-11 22:17:38 +0900

    Fix mistyped classname

--- php/footer.php
+++ php/footer.php
@@ -1,5 +1,5 @@
 <footer class="container is-max-desktop has-background-inherit <?php if ( $WHERE_AM_I == 'page' ): ?>is-max-tablet page-footer<?php endif ?>">
-  <div class="is-flex is-flex-dorection-row is-flex-wrap-wrap has-text-centered copyright">
+  <div class="is-flex is-flex-direction-row is-flex-wrap-wrap has-text-centered copyright">
     <p class="footer-credit mr-3"><?php echo $site->footer(); ?></p>
     <p class="template-credit">Powered by <a class="has-text-inherit has-text-weight-semibold" href="https://www.bludit.com/" target="blank">Bludit</a></p>
   </div>

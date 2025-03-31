6790f43c1775c3551b9a7e351b2fd0fbcd5db33c
sakanafurai <info@via.rossa.cc>
2025-03-31 19:10:22 +0900

    Separate mobile navbar

--- php/sidebar.php
+++ php/sidebar.php
@@ -1,59 +1,74 @@
 <!-- Spacer -->
 <div class="column is-1 has-background-grey-light p-0 is-hidden-mobile spacer"></div>
 
-<!-- Sidebar -->
-<div class="column is-3 has-background-grey-light p-0 sidebar">
-  <div class="sidebar-content">
-
-    <!-- Mobile menu -->
-    <nav class="navbar is-hidden-desktop is-hidden-tablet has-background-inherit">
-      <div class="navbar-brand">
-        <div class="navbar-content">
-          <a role="button" class="navbar-burger has-text-grey m-0" data-target="navMenu" aria-label="menu" aria-expanded="false">
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-          </a>
-        </div>
+<!-- Mobile Sidebar -->
+<div class="column is-3 has-background-grey-light p-0 is-hidden-tablet sidebar">
+  <!-- Mobile menu -->
+  <nav class="navbar is-hidden-desktop has-background-inherit">
+    <div class="navbar-brand">
+      <div class="navbar-content">
+        <a role="button" class="navbar-burger has-text-grey m-0" data-target="navMenu" aria-label="menu" aria-expanded="false">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
       </div>
+    </div>
 
-      <div class="navbar-menu has-background-inherit" id="navMenu">
-        <div class="navbar-end">
-          <a class="navbar-item has-text-inherit" href="<?php echo Theme::siteUrl() ?>"><?php echo $L->get('home'); ?></a>
+    <div class="navbar-menu has-background-inherit" id="navMenu">
+      <div class="navbar-end">
+        <a class="navbar-item has-text-inherit" href="<?php echo Theme::siteUrl() ?>"><?php echo $L->get('home'); ?></a>
 
-          <!-- Categories -->
-          <?php
-          $categoryItems = getCategories();
-          foreach ($categoryItems as $category) { ?>
-            <a class="navbar-item" href="<?php echo $category->permalink() ?>"><?php echo $category->name() ?></a>
-            <?php } ?>
+      <!-- Categories -->
+      <?php
+      $categoryItems = getCategories();
+      foreach ($categoryItems as $category) { ?>
+        <a class="navbar-item" href="<?php echo $category->permalink() ?>"><?php echo $category->name() ?></a>
+        <?php } ?>
 
-          <!-- Static pages -->
-          <?php foreach ($staticContent as $staticPage) : ?>
+        <!-- Static pages -->
+        <?php foreach ($staticContent as $staticPage) : ?>
           <a class="navbar-item" href="<?php echo $staticPage->permalink() ?>"><?php echo $staticPage->title() ?></a>
-          <?php endforeach ?>
+        <?php endforeach ?>
 
-          <div class="navbar-item">
+        <div class="navbar-item">
           <span class="help">Social</span>
 
-            <!-- Social Networks -->
-            <div class="is-flex social-links is-flex-direction-row mt-2">
+          <!-- Social Networks -->
+          <div class="is-flex social-links is-flex-direction-row mt-2">
 
-              <?php foreach (Theme::socialNetworks() as $key => $label) : ?>
-              <a class="mr-3" href="<?php echo $site->{$key}(); ?>" target="_blank"><img class="image is-24x24" src="<?php echo DOMAIN_THEME . 'img/social-icons/' . $key . '.svg' ?>" alt="<?php echo $label ?>"/></a>
-              <?php endforeach; ?>
+          <?php foreach (Theme::socialNetworks() as $key => $label) : ?>
+          <a class="mr-3" href="<?php echo $site->{$key}(); ?>" target="_blank"><img class="image is-24x24" src="<?php echo DOMAIN_THEME . 'img/social-icons/' . $key . '.svg' ?>" alt="<?php echo $label ?>"/></a>
+          <?php endforeach; ?>
 
-              <!-- RSS -->
-              <?php if (Theme::rssUrl()): ?>
-              <a class="level-item" href="<?php echo Theme::rssUrl() ?>" target="_blank">RSS</a>
-              <?php endif; ?>
-            </div>
+          <!-- RSS -->
+          <?php if (Theme::rssUrl()): ?>
+            <a class="level-item" href="<?php echo Theme::rssUrl() ?>" target="_blank">RSS</a>
+          <?php endif; ?>
           </div>
-
         </div>
+
       </div>
-    </nav>
+    </div>
+  </nav>
+
+  <!-- Title logo -->
+  <div class="is-flex is-flex-direction-column is-justify-content-center is-align-items-center">
+    <figure class="title-logo">
+    <?php if ($site->logo()): ?>
+      <img class="image is-rounded" src="<?php echo ($site->logo()); ?>" alt="<?php echo $site->title(); ?>">
+    <?php endif ?>
+    </figure>
+
+    <h1 class="title has-text-centered site-title"><a class="has-text-inherit" href="<?php echo Theme::siteUrl() ?>"><?php echo $site->title(); ?></a></h1>
+
+  </div>
+</div>
+
+<!-- Sidebar -->
+<div class="column is-3 has-background-grey-light p-0 is-hidden-mobile sidebar">
+  <div class="sidebar-content">
 
     <!-- Title logo -->
     <div class="is-flex is-flex-direction-column is-justify-content-center is-align-items-center">

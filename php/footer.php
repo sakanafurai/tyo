7c62203082490fe756b007ff37e2c20bb70206a2
sakanafurai <info@via.rossa.cc>
2025-03-31 19:20:43 +0900

    Add mobile footer and comment out related style settings

--- php/footer.php
+++ php/footer.php
@@ -1,4 +1,13 @@
-<footer class="container is-max-desktop has-background-inherit <?php if ( $WHERE_AM_I == 'page' ): ?>is-max-tablet page-footer<?php endif ?>">
+<!-- Mobile Footer -->
+<footer class="container is-max-desktop has-background-inherit is-hidden-tablet m-0 py-5 px-4 <?php if ( $WHERE_AM_I == 'page' ): ?>is-max-tablet page-footer<?php endif ?>">
+  <div class="is-flex is-flex-direction-row is-flex-wrap-wrap has-text-centered copyright">
+    <p class="footer-credit mr-3"><?php echo $site->footer(); ?></p>
+    <p class="template-credit">Powered by <a class="has-text-inherit has-text-weight-semibold" href="https://www.bludit.com/" target="blank">Bludit</a></p>
+  </div>
+</footer>
+
+<!-- Desktop Footer -->
+<footer class="container is-max-desktop has-background-inherit is-hidden-mobile m-0 py-5 <?php if ( $WHERE_AM_I == 'page' ): ?>is-max-tablet page-footer<?php endif ?>">
   <div class="is-flex is-flex-direction-row is-flex-wrap-wrap has-text-centered copyright">
     <p class="footer-credit mr-3"><?php echo $site->footer(); ?></p>
     <p class="template-credit">Powered by <a class="has-text-inherit has-text-weight-semibold" href="https://www.bludit.com/" target="blank">Bludit</a></p>

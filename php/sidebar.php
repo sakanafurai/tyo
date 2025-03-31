42ead75004b964b61602824dfae5e4b3f282520a
sakanafurai <info@via.rossa.cc>
2025-03-31 19:26:48 +0900

    Move mobile menu to the bottom of header

--- php/sidebar.php
+++ php/sidebar.php
@@ -16,41 +16,6 @@
       </div>
     </div>
 
-    <div class="navbar-menu has-background-inherit" id="navMenu">
-      <div class="navbar-end">
-        <a class="navbar-item has-text-inherit" href="<?php echo Theme::siteUrl() ?>"><?php echo $L->get('home'); ?></a>
-
-      <!-- Categories -->
-      <?php
-      $categoryItems = getCategories();
-      foreach ($categoryItems as $category) { ?>
-        <a class="navbar-item" href="<?php echo $category->permalink() ?>"><?php echo $category->name() ?></a>
-        <?php } ?>
-
-        <!-- Static pages -->
-        <?php foreach ($staticContent as $staticPage) : ?>
-          <a class="navbar-item" href="<?php echo $staticPage->permalink() ?>"><?php echo $staticPage->title() ?></a>
-        <?php endforeach ?>
-
-        <div class="navbar-item">
-          <span class="help">Social</span>
-
-          <!-- Social Networks -->
-          <div class="is-flex social-links is-flex-direction-row mt-2">
-
-          <?php foreach (Theme::socialNetworks() as $key => $label) : ?>
-          <a class="mr-3" href="<?php echo $site->{$key}(); ?>" target="_blank"><img class="image is-24x24" src="<?php echo DOMAIN_THEME . 'img/social-icons/' . $key . '.svg' ?>" alt="<?php echo $label ?>"/></a>
-          <?php endforeach; ?>
-
-          <!-- RSS -->
-          <?php if (Theme::rssUrl()): ?>
-            <a class="level-item" href="<?php echo Theme::rssUrl() ?>" target="_blank">RSS</a>
-          <?php endif; ?>
-          </div>
-        </div>
-
-      </div>
-    </div>
   </nav>
 
   <!-- Title logo -->
@@ -64,6 +29,43 @@
     <h1 class="title has-text-centered site-title mb-5"><a class="has-text-inherit" href="<?php echo Theme::siteUrl() ?>"><?php echo $site->title(); ?></a></h1>
 
   </div>
+
+  <!-- Menu -->
+  <div class="navbar-menu has-background-inherit" id="navMenu">
+    <div class="navbar-end">
+      <a class="navbar-item has-text-inherit" href="<?php echo Theme::siteUrl() ?>"><?php echo $L->get('home'); ?></a>
+
+    <!-- Categories -->
+    <?php
+    $categoryItems = getCategories();
+    foreach ($categoryItems as $category) { ?>
+        <a class="navbar-item" href="<?php echo $category->permalink() ?>"><?php echo $category->name() ?></a>
+      <?php } ?>
+
+      <!-- Static pages -->
+      <?php foreach ($staticContent as $staticPage) : ?>
+        <a class="navbar-item" href="<?php echo $staticPage->permalink() ?>"><?php echo $staticPage->title() ?></a>
+      <?php endforeach ?>
+
+        <div class="navbar-item">
+          <span class="help">Social</span>
+
+          <!-- Social Networks -->
+          <div class="is-flex social-links is-flex-direction-row mt-2">
+
+          <?php foreach (Theme::socialNetworks() as $key => $label) : ?>
+            <a class="mr-3" href="<?php echo $site->{$key}(); ?>" target="_blank"><img class="image is-24x24" src="<?php echo DOMAIN_THEME . 'img/social-icons/' . $key . '.svg' ?>" alt="<?php echo $label ?>"/></a>
+          <?php endforeach; ?>
+
+          <!-- RSS -->
+          <?php if (Theme::rssUrl()): ?>
+            <a class="level-item" href="<?php echo Theme::rssUrl() ?>" target="_blank">RSS</a>
+          <?php endif; ?>
+          </div>
+        </div>
+
+      </div>
+    </div>
 </div>
 
 <!-- Sidebar -->
